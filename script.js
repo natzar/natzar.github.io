c88b17b8b34afccd9cd5efce8cbbfd547cc2c122
beto <hello@96levels.com>
2016-08-24 11:42:45 +0200

    fixs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var quotes=["Check <a href='//natzar.co'>natzar.co</a> and tell me what you think","The best place on earth to work is <a href='http://www.hubud.org' target='_blank'>Hubud</a>","You have monkey mind, you are always busy like a monkey","Nothing is more usefull than Knowledge"];function randomQuote(){return quotes[Math.floor(Math.random()*quotes.length)];}
+var quotes=["Check <a href='//natzar.co'>natzar.co</a> and tell me what you think","The best place on earth to work is <a href='http://www.hubud.org' target='_blank'>Hubud</a>","You have monkey mind, you are always busy like a monkey","Nothing is more useful than Knowledge"];function randomQuote(){return quotes[Math.floor(Math.random()*quotes.length)];}
 
 $('#footQuote').html('"'+ randomQuote()+'"');
 
